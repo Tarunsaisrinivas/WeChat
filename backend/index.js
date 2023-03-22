92dcf01834dcd76e7f75a04950e8e4bef7a946d9
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2023-03-22 19:32:16 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173', // Replace with your frontend URL
+    credentials: true, // Enable cookies for cross-origin requests
+  }));
+
+app.post("/authenticate", async (req, res) => {
+    const { username } = req.body;
+    // Get or create user on Chat Engine!
+    try {
+      const r = await axios.put(
+        "https://api.chatengine.io/users/",
+        { username: username, secret: username, first_name: username },
+        { headers: { "Private-Key": "615a131c-3ed2-446f-a6a2-35e2e0b59f75" } }
+      );
+      return res.status(r.status).json(r.data);
+    } catch (e) {
+      return res.status(e.response.status).json(e.response.data);
+    }
+  });
+
+app.listen(3001);
